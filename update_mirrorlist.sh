ec01340eaf4cb8559a78c93c83eaf629c044228a
Amber J <114687181+arcanamber@users.noreply.github.com>
2023-03-31 22:12:35 -0700

    Update update_mirrorlist.sh

--- update_mirrorlist.sh
+++ update_mirrorlist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo curl 'https://archlinux.org/mirrorlist/?country=US&protocol=http&protocol=https&ip_version=4' > /etc/pacman.d/mirrorlist
-sudo perl -pi -e 's/^#S/S/g' /etc/pacman.d/mirrorlist
+sudo sed -i 's/^#S/S/g' /etc/pacman.d/mirrorlist
 
 date_in_gmt=$(TZ="GMT" date "+%Y-%m-%d")
 mirrorlist=$(cat /etc/pacman.d/mirrorlist)
